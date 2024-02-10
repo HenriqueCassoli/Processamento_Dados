c85a6254aefc3792dc1dc3cfc8f420dfbf98ebdb
Cassoli <fernandes.cassoli@gmail.com>
2024-02-10 12:05:26 -0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        int x,y ;
+
+        x=5;
+        y=2*x;
+
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
